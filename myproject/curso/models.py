63432c5b56f28cad072f65ebcd0dedd6312ab6aa
luriadias <56274236+luriadias@users.noreply.github.com>
2022-05-21 15:41:00 -0300

    Add files via upload

---
+++ myproject/curso/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User 
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length= 255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+#slug serve para para acessar cada post _tipo urls_ cada post tem um slug 
+
